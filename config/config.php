7a73f8e89f7704fad78b4c583797dc2c4481cb05
Philipp Daun <post@philippdaun.net>
2024-11-10 18:14:04 +0100

    Add config comments

--- config/config.php
+++ config/config.php
@@ -1,11 +1,22 @@
 <?php
 
 return [
+
+    // The URL to ping
     'url' => env('UPTIME_PING_URL', null),
+
+    // The HTTP method to use
     'method' => env('UPTIME_PING_METHOD', 'GET'),
+
+    // The number of retries before failing
     'retries' => 3,
-    'timeout' => 3, // seconds
+
+    // The timeout in seconds before failing
+    'timeout' => 3,
+
+    // Additional headers to send with the request
     'headers' => [
         // 'User-Agent' => 'Laravel Uptime Ping',
     ],
+
 ];
