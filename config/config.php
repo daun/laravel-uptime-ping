8694c06d4ca37292e0f0240f87f642cb24341199
Philipp Daun <post@philippdaun.net>
2024-11-10 18:14:12 +0100

    Add cron frequency setting

--- config/config.php
+++ config/config.php
@@ -8,6 +8,9 @@ return [
     // The HTTP method to use
     'method' => env('UPTIME_PING_METHOD', 'GET'),
 
+    // The frequency at which to ping the URL, in crontab syntax (see https://crontab.guru)
+    'cron' => env('UPTIME_PING_CRON', '* * * * *'),
+
     // The number of retries before failing
     'retries' => 3,
 
