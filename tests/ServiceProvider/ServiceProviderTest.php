ca16d440d8fd40d0aa770c4a057380eadf1f13d2
Philipp Daun <post@philippdaun.net>
2024-11-11 18:21:51 +0100

    Finish service provider tests

--- tests/ServiceProvider/ServiceProviderTest.php
+++ tests/ServiceProvider/ServiceProviderTest.php
@@ -5,8 +5,23 @@ use Illuminate\Console\Scheduling\Schedule;
 it('adds a new job to the queue', function () {
     $schedule = Mockery::mock(Schedule::class);
     $schedule->shouldReceive('job')->once()->with('Daun\LaravelUptimePing\Jobs\UptimePing')->andReturnSelf();
+    $schedule->shouldReceive('when')->once()->with(null)->andReturnSelf();
+    $schedule->shouldReceive('everyMinute')->once()->andReturnSelf();
+
+    $this->app->instance(Schedule::class, $schedule);
+
+    $this->bootServiceProvider();
+});
+
+it('makes the job conditional on the url being defined', function () {
+    config(['uptime-ping.url' => 'https://example.net']);
+
+    $schedule = Mockery::mock(Schedule::class);
+    $schedule->shouldReceive('job')->once()->with('Daun\LaravelUptimePing\Jobs\UptimePing')->andReturnSelf();
     $schedule->shouldReceive('when')->once()->with('https://example.net')->andReturnSelf();
     $schedule->shouldReceive('everyMinute')->once()->andReturnSelf();
+
+    $this->app->instance(Schedule::class, $schedule);
 
     $this->bootServiceProvider();
 });
